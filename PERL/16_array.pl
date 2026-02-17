da3d5c326d9bee14cc163c6cbd5a7201266160ad
Soumik-180 <soumikray430@gmail.com>
2026-02-17 12:30:02 +0530

    adding array file

---
+++ PERL/16_array.pl
@@ -0,0 +1,28 @@
+my @array = ("Soumik", "Ronan", "Abhay");
+
+print $array[0];
+print "\n";
+print @array;
+
+push(@array, "Vanshika");
+print "\n";
+print @array;
+
+pop(@array);
+print "\n";
+print @array;
+
+# my $name = "Soumik";
+
+# my @array = split(//,$name);
+# print $array[0];
+# print "\n";
+# print $array[1];
+# print "\n";
+# print $array[2];
+# print "\n";
+# print $array[3];
+# print "\n";
+# print $array[4];
+# print "\n";
+# print $array[5];
